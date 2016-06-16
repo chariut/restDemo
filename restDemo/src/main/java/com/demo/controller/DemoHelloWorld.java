7a7838d0a7e38612ea6d77211508743bb64abe9a
chariut <govindachari@gmail.com>
2016-06-16 16:27:36 -0500

    codefix

--- restDemo/src/main/java/com/demo/controller/DemoHelloWorld.java
+++ restDemo/src/main/java/com/demo/controller/DemoHelloWorld.java
@@ -1,5 +1,6 @@
 package com.demo.controller;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,9 @@ public class DemoHelloWorld {
 		String message = "<br><div style='text-align:center;'>"
 				+ "<h3>********** Hello World, Spring MVC Demo</h3>This message is coming from DemoHelloWorld.java **********</div><br><br>";
 		return new ModelAndView("welcome", "message", message);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHelloWorld.class, args);
 	}
 }
